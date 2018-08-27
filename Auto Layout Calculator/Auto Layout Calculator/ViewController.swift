f1c7f930011107e22718ea11857077df85367a70
Neota Moe <neota.moe@gmail.com>
2018-08-26 19:15:07 -0500

    changes to layout

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Neota Moe on 8/26/18.
+//  Copyright © 2018 Neota Moe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+
+}
+
