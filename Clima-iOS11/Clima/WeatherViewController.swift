dec175ce6660261fae3526351e7f113537d0ae18
Neota Moe <neota.moe@gmail.com>
2018-08-28 04:31:32 -0500

    weather api request made and updating display

--- Clima-iOS11/Clima/WeatherViewController.swift
+++ Clima-iOS11/Clima/WeatherViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
@@ -15,7 +17,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "6c5439708b087cb3b2ea40966090f587"
-    
+    let weatherDataModel = WeatherDataModel()
 
     //TODO: Declare instance variables here
     let locationManager = CLLocationManager()
@@ -35,6 +37,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
       
     }
     
@@ -44,8 +47,20 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
-
+    func getWeatherData(url: String, parameters: [String:String]){
+      Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+        response in
+        if response.result.isSuccess {
+          print("Success! Got the weather data!")
+          let weatherJSON : JSON = JSON(response.result.value!)
+          self.updateWeatherData(json: weatherJSON)
+        } else {
+          print("Error \(String(describing: response.result.error))")
+          self.cityLabel.text = "Connection Issues"
+        }
+      }
+    }
+  
     
     
     
@@ -56,7 +71,19 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
    
     
     //Write the updateWeatherData method here:
-    
+  func updateWeatherData(json:JSON) {
+    if let tempResult = json["main"]["temp"].double {
+      weatherDataModel.temperature = Int(tempResult - 273.15)
+
+      weatherDataModel.city = json["name"].stringValue
+      weatherDataModel.condition = json["weather"][0]["id"].intValue
+      weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+      
+      updateUIWithWeatherData()
+    } else {
+      cityLabel.text = "Weather Unavailable"
+    }
+  }
 
     
     
@@ -66,6 +93,11 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the updateUIWithWeatherData method here:
+    func updateUIWithWeatherData() {
+      cityLabel.text = weatherDataModel.city
+      temperatureLabel.text = "\(weatherDataModel.temperature)"
+      weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
     
     
     
@@ -77,11 +109,30 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      let location = locations[locations.count - 1]
+      if location.horizontalAccuracy > 0 {
+        locationManager.stopUpdatingLocation()
+        locationManager.delegate = nil  // this stops locationManager from receiving more data while it is stopping
+        
+        print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+        
+        let latitude = String(location.coordinate.latitude)
+        let longitude = String(location.coordinate.longitude)
+        
+        let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+
+        getWeatherData(url: WEATHER_URL, parameters: params)
+
+      }
+      
+    }
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+      print(error)
+      cityLabel.text = "Location Unavailable"
+    }
     
     
 
