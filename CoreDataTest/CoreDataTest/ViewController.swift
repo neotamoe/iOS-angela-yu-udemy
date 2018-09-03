1a9744686af7bed10bddb473db2143a144c2b02d
Neota Moe <neota.moe@gmail.com>
2018-09-03 13:12:08 -0500

    removed item class, set up start of coredata

---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Neota Moe on 9/3/18.
+//  Copyright © 2018 Neota Moe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
