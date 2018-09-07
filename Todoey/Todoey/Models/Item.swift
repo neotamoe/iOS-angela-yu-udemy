0e6d06c2ff593ea04d2d46280e065f2c5f73e32e
Neota Moe <neota.moe@gmail.com>
2018-09-07 18:41:30 -0500

    querying data using realm

--- Todoey/Todoey/Models/Item.swift
+++ Todoey/Todoey/Models/Item.swift
@@ -13,6 +13,7 @@ class Item: Object {
   
   @objc dynamic var title : String = ""
   @objc dynamic var done : Bool = false
+  @objc dynamic var dateCreated : Date?
  
   // define inverse relationship
   var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
